fff636b5af5412fd8025fb8ba8d95466cf67a731
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-10-14 14:01:21 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,15 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage =randomNumber(playerAttack-3, playerAttack);
+    enemyHealth = Math.max (0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +54,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max (0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -87,7 +89,7 @@ var startGame = function(){
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40,60);
 
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
@@ -124,17 +126,14 @@ var endGame = function(){
   var playAgainConfirm = window.confirm("Would you like to play again?");
   if (playAgainConfirm){
     //restart game
-    startGame();
+    
   }
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!")
   }
 };
-
-  //play again
-  startGame()
+startGame();  
 };
-
 
 //function to enter the shop 
 var shop = function(){
@@ -184,4 +183,8 @@ var shop = function(){
 // start the game when the page loads 
 startGame();
 
-
+// function to generate a random numeric value 
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max-min+1))+ min;
+  return value;
+};
